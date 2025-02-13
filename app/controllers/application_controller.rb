ba862e5d4686487c081b5783c28d3aa346f782f1
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-13 23:18:28 +0100

    Update application controller to redirect users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_dashboard_path # Redirect to the admin dashboard
+    elsif resource.author?
+      authors_dashboard_path # Redirect to the authors dashboard
+    else
+      root_path # Redirect to the root path for listeners or other roles
+    end
+  end
 end
