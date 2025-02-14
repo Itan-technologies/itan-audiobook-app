21e0c278482a4411a101a8d71669e33e44aab816
Ol-create <oluola96@gmail.com>
2025-02-14 11:48:38 +0100

    add JWT module to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
   enum role: { listener: 'listener', author: 'author', admin: 'admin' }
   after_initialize :set_default_role, if: :new_record?
