fdf7ba723c1c1c5d2e16f851db9c5d80bacfc24d
Ol-create <oluola96@gmail.com>
2025-02-14 12:36:48 +0100

    add user controllers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,9 +312,10 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.secret_key_base
-    jwt.dispatch_key = ['Authorization']
-    jwt.revocation_requests_key = ['Authorization']
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [['POST', %r{^/login$}]]
+    jwt.revocation_requests = [['DELETE', %r{^/logout$}]]
+    jwt.expiration_time = 3600
   end
 
 end
