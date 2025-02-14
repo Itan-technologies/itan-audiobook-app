d56dc56bf8f5a855d2a6d1e741547be00556ff2b
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-14 19:22:06 +0100

    Include confirmable & skip for admins

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,35 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   enum role: { listener: 'listener', author: 'author', admin: 'admin' }
   after_initialize :set_default_role, if: :new_record?
+  before_create :skip_confirmation_for_admin
+
+  # Custom method to check if the user can sign in
+  def active_for_authentication?
+    super && (admin? || confirmed?)
+  end
+
+  # Custom message for unconfirmed authors
+  def inactive_message
+    if author? && !confirmed?
+      :unconfirmed
+    else
+      super
+    end
+  end
 
   private
 
   def set_default_role
     self.role ||= :author
   end
+
+  def skip_confirmation_for_admin
+  Rails.logger.debug "Skipping confirmation for admin user" if admin?
+  skip_confirmation! if admin?
+end
 end
