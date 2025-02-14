b289adb4b45ec6a5a27e3c9d7de9422eda8d2158
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-14 19:21:37 +0100

    Configure permitted parameters for sign up & sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
     if resource.admin?
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
     else
       root_path # Redirect to the root path for listeners or other roles
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
