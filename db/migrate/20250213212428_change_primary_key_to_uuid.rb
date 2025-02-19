3830d7c777b699f89b4806b20b0726558f7365b6
Ol-create <oluola96@gmail.com>
2025-02-19 04:47:20 +0100

    fix linter err

--- db/migrate/20250213212428_change_primary_key_to_uuid.rb
+++ db/migrate/20250213212428_change_primary_key_to_uuid.rb
@@ -1,39 +1,49 @@
 # frozen_string_literal: true
 
-class ChangePrimaryKeyToUuid < ActiveRecord::Migration[7.1]
-  def change
-    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+def up
+  enable_pgcrypto
+  create_users_table
+  copy_old_users_data
+  rename_tables
+  drop_old_users_table
+end
 
-    # Create a new table with UUID as primary key
-    create_table :new_users, id: :uuid do |t|
-      t.string :email, default: '', null: false
-      t.string :encrypted_password, default: '', null: false
-      t.string :reset_password_token
-      t.datetime :reset_password_sent_at
-      t.datetime :remember_created_at
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
-      t.string :role
-      t.index :email, unique: true
-      t.index :reset_password_token, unique: true
-    end
+private
 
-    # Copy data from old table to new table
-    execute <<-SQL
-      INSERT INTO new_users (id, email, encrypted_password, reset_password_token, reset_password_sent_at, remember_created_at, created_at, updated_at, role)
-      SELECT gen_random_uuid(), email, encrypted_password, reset_password_token, reset_password_sent_at, remember_created_at, created_at, updated_at, role
-      FROM users;
-    SQL
+def enable_pgcrypto
+  enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+end
 
-    # Rename old table and new table
-    rename_table :users, :old_users
-    rename_table :new_users, :users
-
-    # Drop old table
-    drop_table :old_users
+def create_users_table
+  create_table :new_users, id: :uuid do |t|
+    t.string :email, default: '', null: false
+    t.string :encrypted_password, default: '', null: false
+    t.string :reset_password_token
+    t.datetime :reset_password_sent_at
+    t.datetime :remember_created_at
+    t.datetime :created_at, null: false
+    t.datetime :updated_at, null: false
+    t.string :role
+    t.index :email, unique: true
+    t.index :reset_password_token, unique: true
   end
+end
 
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
+def copy_old_users_data
+  execute <<-SQL
+    INSERT INTO new_users (id, email, encrypted_password, reset_password_token, reset_password_sent_at,#{' '}
+                           remember_created_at, created_at, updated_at, role)
+    SELECT gen_random_uuid(), email, encrypted_password, reset_password_token, reset_password_sent_at,#{' '}
+           remember_created_at, created_at, updated_at, role
+    FROM users;
+  SQL
+end
+
+def rename_tables
+  rename_table :users, :old_users
+  rename_table :new_users, :users
+end
+
+def drop_old_users_table
+  drop_table :old_users
 end
