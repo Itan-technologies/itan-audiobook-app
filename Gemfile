aae4cf3c3017a64e64c90bba04715e6b0e53ac23
Ol-create <oluola96@gmail.com>
2025-02-19 05:44:56 +0100

    fix linter err

--- Gemfile
+++ Gemfile
@@ -67,7 +67,7 @@ group :development do
   # gem "spring"
 end
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
 
 gem 'devise-jwt'
 
--- app/controllers/authors/dashboard_controller.rb
+++ app/controllers/authors/dashboard_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Authors
   class DashboardController < ApplicationController
     before_action :authenticate_user!
