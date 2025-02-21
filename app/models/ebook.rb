c8f9d14a74775e2ac78a95ff9905cf688bff1e70
Ayokunnumi1 <omololuayk@gmail.com>
2025-02-21 12:15:49 +0100

    Update ebook model with the action for generating unique id

--- app/models/ebook.rb
+++ app/models/ebook.rb
@@ -1,8 +1,19 @@
 class Ebook < ApplicationRecord
+  before_create :generate_unique_ebook_id
+
   has_and_belongs_to_many :users, join_table: 'users_ebooks'
   belongs_to :audiobook
   has_one :audiobook
   has_many :reviews
   has_many :purchases
   has_many :chapters
+  
+  private
+
+  def generate_unique_ebook_id
+    prefix = 'BOO'
+    last_ebook = Ebook.order(:created_at).last
+    last_number = last_ebook&.unique_ebook_id&.gsub(prefix, '')&.to_i || 1000
+    self.unique_ebook_id = "#{prefix}#{last_number + 1}"
+  end
 end
